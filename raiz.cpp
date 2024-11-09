3d934a1e8708c2716f4d12c81fa1697713f27847
pedrohgdp <pedro.henrique.g.paula@gmail.com>
2024-11-09 15:16:00 -0300

    Raizer ADD

---
+++ raiz.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    //s×s≡x (mod p)
+
+    //Variavel do numero 
+    long long p;
+    cin >> p;
+
+    vector<long long> raizes(p, -1);
+
+    raizes[0] = 0;
+
+    for(int x = 1; x < p; x++){
+        for(int s = 0; s < p; s++){
+            if((s * s) % p == x){
+                if(raizes[x] == -1 || s < raizes[x]){
+                    raizes[x] = s;
+                }
+            }
+        }
+    }
+
+
+    for(int i = 0; i < p; i++){
+        cout << raizes[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
