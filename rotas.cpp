f1751eac51ba8f9f1ecc8409dc343dc9ed4b3171
pedrohgdp <pedro.henrique.g.paula@gmail.com>
2024-11-17 20:14:30 -0300

    Att rotas

--- rotas.cpp
+++ rotas.cpp
@@ -1,80 +1,105 @@
+/*PROBLEM: 
+
+
+Aphrodite is concerned about humanity's love lives. Her original plan was for everyone
+in a group of N people to love one another. However, this ideal scenario hasn't 
+materialized. For instance, Zé loves Maria, who loves Sebastião, who loves Carolina.
+Your task is to create a program that determines if "total love" exists within a given
+group. This means checking if every pair of people in the group loves each other reciprocally.
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef vector<int> vi;
 
-void dfs(int u, vector<vi> &grafoNormal, vi &visitados, stack<int> &pilha){
-    visitados[u] = 1;
-    for(int v : grafoNormal[u]){
-        if(!visitados[v]){
-            dfs(v, grafoNormal, visitados, pilha);
+//NORMAL DFS BUT WE PUT U ON THE STACK AT THE END.
+void dfs(int u, vector<vi> &normalGraph, vi &visited, stack<int> &pilha){
+    visited[u] = 1;
+    for(int v : normalGraph[u]){
+        if(!visited[v]){
+            dfs(v, normalGraph, visited, pilha);
         }
     }
     pilha.push(u);
 }
 
 
-
-void reverseDfs(int u, vector<vi> &grafoInvertido, vi &visitados, vi &scc){
-    visitados[u] = 1;
+//NORMAL DFS BUT IN INVERSE GRAPH
+void reverseDfs(int u, vector<vi> &invertedGraph, vi &visited, vi &scc){
+    visited[u] = 1;
     scc.push_back(u);
-    for(int v : grafoInvertido[u]){
-        if(!visitados[v]){
-            reverseDfs(v, grafoInvertido, visitados, scc);
+    for(int v : invertedGraph[u]){
+        if(!visited[v]){
+            reverseDfs(v, invertedGraph, visited, scc);
         }
     }
 }
 
-
-
-
 int main(){
 
-    int nv, m;
+    //WE CREATE THE NUMBER OF VERTICES AND NUMBER OF RELATIONSHIPS
+    int nv, m; 
+    //WE READ NV AND M AND DO THAT WHILE NV AND M IS DIFFERENT FROM 0
     while(cin >> nv >> m, nv != 0 && m != 0){
-        vector<vi> grafoNormal(nv);
-        vector<vi> grafoInvertido(nv);
+        //WE CREATE THE TWO GRAPHS NORMAL AND INVERTED
+        vector<vi> normalGraph(nv);
+        vector<vi> invertedGraph(nv);
 
-
+        //CREATE VERTICES U AND V AND R
+        //R == 1 RESULT IN U LOVES V 
+        //R == 2 RESULT IN U LOVES V AND V LOVES U
         int u, v, r;
-        for(int i = 0; i < m; i++){
+        for(int i = 0; i < m; i++){ 
+            //WE READ U V AND R
             cin >> u >> v >> r;
             if(r == 1){
-                grafoNormal[u - 1].push_back(v - 1);
-                grafoInvertido[v - 1].push_back(u - 1);
+                //AND WE PUT THE LOGIC OF R == 1 OR R == 2
+                normalGraph[u - 1].push_back(v - 1);
+                invertedGraph[v - 1].push_back(u - 1);
             }
             if(r == 2){
-                grafoNormal[u - 1].push_back(v - 1);
-                grafoInvertido[u - 1].push_back(v - 1);
-                grafoNormal[v - 1].push_back(u - 1);
-                grafoInvertido[v - 1].push_back(u - 1);
+                normalGraph[u - 1].push_back(v - 1);
+                invertedGraph[u - 1].push_back(v - 1);
+                normalGraph[v - 1].push_back(u - 1);
+                invertedGraph[v - 1].push_back(u - 1);
             }
         }
 
-        vi visitados(nv, 0);
+        //WE CREATE A VECTOR OF INTEGERS TO MARK THE VERTICES VISITED
+        vi visited(nv, 0);
+
+        //WE CREATE THE STACK
         stack<int> pilha;
+        //WE FILL THE STACK USING DFS
         for(int i = 0; i < nv; i++){
-            if(!visitados[i]){
-                dfs(i, grafoNormal, visitados, pilha);
+            if(!visited[i]){
+                dfs(i, normalGraph, visited, pilha);
             }
         }
 
-        fill(visitados.begin(), visitados.end(), 0);
-        bool amorTotal = false;
+        //WE MARK ALL VERTICES AS NOT VISITED
+        fill(visited.begin(), visited.end(), 0);
+        //WE CREATE TOTAL LOVE VARIABLE 
+        bool totalLove = false;
+        //WE USE DFS ON INVERSE GRAPH TO GET THE SCC OF THE GRAPH
         while(!pilha.empty()){
             int u = pilha.top();
             pilha.pop();
-            if(!visitados[u]){
+            if(!visited[u]){
+                //VECTOR WITH SCCS
                 vi scc;
-                reverseDfs(u, grafoInvertido, visitados, scc);
+                reverseDfs(u, invertedGraph, visited, scc);
                 if(scc.size() == nv){
-                    amorTotal = true;
+                    totalLove = true;
                     break;
                 }
             }
         }
 
-        cout << (amorTotal ? 1 : 0) << endl;
+        //IF TOTALLOVES IS TRUE WE PRINT 1 ELSE WE PRINT 0
+        cout << (totalLove ? 1 : 0) << endl;
     }
     return 0;
 }
